8c9f794a88584e9ca2006719d35f8e0f5680f2bb
Valentin Burkin <burkinman@me.com>
2018-04-10 13:54:40 +0300

    test: Add tests for New Feed Selection

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -98,10 +98,38 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* A test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+        let initialTitle;
+        let initialContent;
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /**
+         * @description Make sure that the feed is loaded
+         */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                // Set values after the first loading
+                initialTitle = $('.header-title').html();
+                initialContent = $('.feed').html();
+                done();
+            });
+        });
+
+        /* A test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        it('content changed on loading', function(done) {
+            loadFeed(1, function() {
+                expect($('.header-title').html()).not.toEqual(initialTitle);
+                expect($('.feed').html()).not.toEqual(initialContent);
+                done();
+            });
+        });
+
+        // Restore to default state
+        afterAll(function() {
+            loadFeed(0);
+        });
+    });
 }());
